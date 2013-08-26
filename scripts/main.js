f8f40d4004affc17fac0a7b9921636180b4ecadf
Oleksiy Radyvanyuk <oradyvanyuk@cogniance.com>
2013-08-26 23:26:40 +0300

    Added mouse down/up reactions for 'Add Task' button

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,50 @@
-function onAddTask()
-{
-	window.alert("Task added!");
+function addClass(element, classToAdd) {
+    var currentClassValue = element.className;
+      
+    if (currentClassValue.indexOf(classToAdd) == -1) {
+        if ((currentClassValue == null) || (currentClassValue === "")) {
+            element.className = classToAdd;
+        } else {
+            element.className += " " + classToAdd;
+        }
+    }
+}
+      
+function removeClass(element, classToRemove) {
+    var currentClassValue = element.className;
+      
+    // removing a class value when there is more than one class value present
+    // and the class you want to remove is not the first one
+    if (currentClassValue.indexOf(" " + classToRemove) != -1) {
+        element.className = element.className.replace(" " + classToRemove, "");
+        return;
+    }
+      
+    // removing the first class value when there is more than one class
+    // value present
+    if (currentClassValue.indexOf(classToRemove + " ") != -1) {
+        element.className = element.className.replace(classToRemove + " ", "");
+        return;
+    }
+      
+    // removing the first class value when there is only one class value 
+    // present
+    if (currentClassValue.indexOf(classToRemove) != -1) {
+        element.className = element.className.replace(classToRemove, "");
+        return;
+    }
+}
+
+function onAddTask() {
+// 	window.alert("Task added!");
+}
+
+function onAddTaskButtonDown() {
+	var theButton = document.querySelector("#add_task");
+	addClass(theButton, "button_pressed");
+}
+
+function onAddTaskButtonUp() {
+	var theButton = document.querySelector("#add_task");
+	removeClass(theButton, "button_pressed");
 }
